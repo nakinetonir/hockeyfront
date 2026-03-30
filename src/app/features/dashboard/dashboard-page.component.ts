66453f6510544ffb4625a1bc5a9245e9e28517ea
nakinetonir <nachorio82@gmail.com>
2026-03-30 22:25:14 +0200

    dom animation

--- src/app/features/dashboard/dashboard-page.component.ts
+++ src/app/features/dashboard/dashboard-page.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { ApiService } from '../../core/services/api.service';
 import { DashboardSummary } from '../../core/models/api.models';
+import { TeamLogoComponent } from '../../shared/components/team-logo/team-logo.component';
+import { isTeam360 } from '../../core/utils/team-branding';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TeamLogoComponent],
   template: `
     <section>
       <h1 class="page-title">Liga Senior 2</h1>
@@ -38,9 +40,9 @@ import { DashboardSummary } from '../../core/models/api.models';
             <table>
               <thead><tr><th>Jugador</th><th>Equipo</th><th>G</th><th>A</th></tr></thead>
               <tbody>
-                <tr *ngFor="let item of data.topScorers">
+                <tr *ngFor="let item of data.topScorers" [class.team-360-row]="is360(item.team)">
                   <td>{{ item.player }}</td>
-                  <td>{{ item.team }}</td>
+                  <td><div class="team-cell"><app-team-logo [team]="item.team" [animate360]="true"></app-team-logo><span class="team-cell-name dashboard-team-name">{{ item.team }}</span></div></td>
                   <td>{{ item.goals || 0 }}</td>
                   <td>{{ item.assists || 0 }}</td>
                 </tr>
@@ -55,9 +57,9 @@ import { DashboardSummary } from '../../core/models/api.models';
             <table>
               <thead><tr><th>Jugador</th><th>Equipo</th><th>A</th><th>Pts</th></tr></thead>
               <tbody>
-                <tr *ngFor="let item of data.topAssisters">
+                <tr *ngFor="let item of data.topAssisters" [class.team-360-row]="is360(item.team)">
                   <td>{{ item.player }}</td>
-                  <td>{{ item.team }}</td>
+                  <td><div class="team-cell"><app-team-logo [team]="item.team" [animate360]="true"></app-team-logo><span class="team-cell-name dashboard-team-name">{{ item.team }}</span></div></td>
                   <td>{{ item.assists || 0 }}</td>
                   <td>{{ item.points || ((item.goals || 0) + (item.assists || 0)) }}</td>
                 </tr>
@@ -72,9 +74,9 @@ import { DashboardSummary } from '../../core/models/api.models';
             <table>
               <thead><tr><th>Portero</th><th>Equipo</th><th>SV%</th><th>Tiros</th></tr></thead>
               <tbody>
-                <tr *ngFor="let item of data.topGoalies">
+                <tr *ngFor="let item of data.topGoalies" [class.team-360-row]="is360(item.team)">
                   <td>{{ item.goalie }}</td>
-                  <td>{{ item.team }}</td>
+                  <td><div class="team-cell"><app-team-logo [team]="item.team" [animate360]="true"></app-team-logo><span class="team-cell-name dashboard-team-name">{{ item.team }}</span></div></td>
                   <td>{{ item.save_pct || 0 | number:'1.0-2' }}</td>
                   <td>{{ item.shots || 0 }}</td>
                 </tr>
@@ -92,5 +94,9 @@ export class DashboardPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.api.getDashboardSummary().subscribe((data) => (this.summary = data));
+  }
+
+  is360(team?: string): boolean {
+    return isTeam360(team);
   }
 }
